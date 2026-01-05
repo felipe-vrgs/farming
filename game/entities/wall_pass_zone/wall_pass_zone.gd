dcde91f6fb3a59ad0f22ee6cbc3c712aedee081e
Felipe Vargas <feliipevrgs@gmail.com>
2026-01-05 14:26:37 -0300

    refactor: wall pass

--- game/entities/wall_pass_zone/wall_pass_zone.gd
+++ game/entities/wall_pass_zone/wall_pass_zone.gd
@@ -1,4 +1,3 @@
-@tool
 class_name WallPassZone
 extends Area2D
 
@@ -13,18 +12,18 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 
+	# Check for bodies already inside
+	for body in get_overlapping_bodies():
+		_on_body_entered(body)
+
 
 func _on_body_entered(body: Node) -> void:
-	if Engine.is_editor_hint():
-		return
-
-	if body is Player:
-		(body as Player).set_terrain_collision(false)
+	if body.is_in_group(Groups.PLAYER) or body is Player:
+		if body.has_method("set_terrain_collision"):
+			body.set_terrain_collision(false)
 
 
 func _on_body_exited(body: Node) -> void:
-	if Engine.is_editor_hint():
-		return
-
-	if body is Player:
-		(body as Player).set_terrain_collision(true)
+	if body.is_in_group(Groups.PLAYER) or body is Player:
+		if body.has_method("set_terrain_collision"):
+			body.set_terrain_collision(true)
